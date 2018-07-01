13c2c232135538e3c37d952e060668692a1a581b
enzoborgfrantz <enzo.frantz@goeuro.com>
2018-07-01 22:23:37 +0200

    add tasks

--- src/tasks.js
+++ src/tasks.js
@@ -7,15 +7,46 @@ const tasks = new Listr([
     task: () =>
       new Listr([
         {
-          title: 'Checking git status',
+          title: 'Check current branch',
           task: () =>
-            execa.stdout('git', ['status', '--porcelain']).then(result => {
-              if (result !== '') {
+            execa
+              .stdout('git', ['symbolic-ref', '--short', 'HEAD'])
+              .then(branch => {
+                if (branch !== 'master') {
+                  throw new Error(
+                    'Not on `master` branch. Use --any-branch to publish anyway.',
+                  );
+                }
+              }),
+        },
+        {
+          title: 'Check local working tree',
+          task: () =>
+            execa.stdout('git', ['status', '--porcelain']).then(status => {
+              if (status !== '') {
                 throw new Error(
                   'Unclean working tree. Commit or stash changes first.',
                 );
               }
             }),
+        },
+        {
+          title: 'Check remote history',
+          task: () =>
+            execa
+              .stdout('git', [
+                'rev-list',
+                '--count',
+                '--left-only',
+                '@{u}...HEAD',
+              ])
+              .then(result => {
+                if (result !== '0') {
+                  throw new Error(
+                    'Remote history differs. Please pull changes.',
+                  );
+                }
+              }),
         },
       ]),
   },
